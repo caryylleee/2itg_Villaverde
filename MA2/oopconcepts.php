ce93a39393cacd2d82fd975754155d3e1031fe2a
caryylleee <carylle.villaverde14@gmail.com>
2023-03-18 19:00:24 +0800

    Update Number 2 with constructors

--- MA2/oopconcepts.php
+++ MA2/oopconcepts.php
@@ -53,20 +53,21 @@ class KalleCafe extends Cake
     private $customerName;
 
     // Customized setters and getters
-    public function setcustomerName($customerName)
+    public function __construct($orderType, $cakeType, $cakePrice, $customerName)
     {
+        $this->orderType = $orderType;
+        $this->cakeType = $cakeType;
+        $this->cakePrice = $cakePrice;
         $this->customerName = $customerName;
-    }
-
-    public function getcustomerName($customerName)
-    {
-        return $this->customerName;
     }
 
     // Additional Method
     function printInfo()
     {
         echo "<br> <br> Your name is: $this->customerName <br>";
+        echo "Your order type is: $this->orderType <br>";
+        echo "The cake that you ordered is: $this->cakeType <br>";
+        echo "Your cake costs: $this->cakePrice <br>";
     }
 }
 
@@ -77,12 +78,7 @@ $cake1->setcakeType("Choco Mousse");
 $cake1->setcakePrice(649);
 $cake1->displayInfo();
 
-$cake2 = new KalleCafe();
-$cake2->setcustomername("Ms. Micole");
-$cake2->setorderType("Dine-in");
-$cake2->setcakeType("A slice of Blueberry Cheesecake");
-$cake2->setcakePrice(135);
+$cake2 = new KalleCafe("Dine-in", "A slice of Blueberry Cheesecake", 649, "Ms. Micole");
 $cake2->printInfo();
-$cake1->displayInfo();
 
 ?>
